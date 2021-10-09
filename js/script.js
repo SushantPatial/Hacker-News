54f51f1ec62497d2263da3b2ceb69f1abbda72d3
Sushant Patial <sushantpatial18@gmai.com>
2021-10-09 21:27:37 +0530

    added nav and footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-function hover(element) {
+function hover(element) 
+{
   element.setAttribute('src', 'images/icons/hover/' + element.className + '.png');
 };
 
-function unhover(element) {
+function unhover(element) 
+{
   element.setAttribute('src', 'images/icons/' + element.className + '.png');
 };
+
+$(".hide-button").click(function()
+{
+  $(this).closest(".content").fadeTo(500, 0.01, function() 
+  { 
+    $(this).slideUp(200, function()
+    {
+        $(this).remove(); 
+    }); 
+  });
+})
