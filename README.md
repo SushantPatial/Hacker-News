954f3217dd47ff65feebf0b0f5c835554fe4a6b5
iter8 <randomdweller.me@gmail.com>
2021-10-10 01:40:06 +0530

    Final Commit (apparently)

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Hacker News
 
-<<<<<<< HEAD
-Hosted at: https://github.com/SushantPatial/Hacker-News.git
-=======
 Hosted at: https://sushantpatial.github.io/Hacker-News/
->>>>>>> bca17de47c46f0690c8c7772ef63a3ac5f45e5c7
--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function hover(element) {
 function unhover(element) {
   element.setAttribute('src', 'images/icons/' + element.className + '.png');
 };
+
+$(".hide-button").click(function()
+{
+  $(this).closest(".content").fadeTo(500, 0.01, function() 
+  { 
+    $(this).slideUp(200, function()
+    {
+      $(this).remove(); 
+    }); 
+  });
+});
